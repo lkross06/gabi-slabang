b2621f8195aa73907f960e142f1ae7f655f9c3f9
lkross06 <lucaskalaniross@gmail.com>
2026-05-07 00:49:56 -0700

    temp: button async interrupt

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,20 @@
 
 Adafruit_SSD1306 SSD1306(SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX, &SPI, SSD1306_DC, SSD1306_RST, SSD1306_CS);
 
-uint8_t i;
+uint64_t i;
+volatile uint64_t i_lastinterrupt;
+
+void IRAM_ATTR handler();
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   i = 0;
+  i_lastinterrupt = 0;
+
+  pinMode(41, INPUT_PULLUP); //GPIO41 --> button --> GND
+  attachInterrupt(digitalPinToInterrupt(41), handler, FALLING);
 
   SPI.begin(SSD1306_CLK, SSD1306_MISO, SSD1306_MOSI, SSD1306_CS);
 
@@ -31,6 +38,13 @@ void loop() {
   SSD1306.setCursor(0,0);
   SSD1306.printf("%d", i);
   SSD1306.display();
-  delay(80);
+  delay(50);
   i++;
+}
+
+void IRAM_ATTR handler() {
+  if (i - i_lastinterrupt < 10) return;
+
+  Serial.printf("Button pressed! i=%d\n", i);
+  i_lastinterrupt = i;
 }
